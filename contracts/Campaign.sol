2c049088d0c0d6077e6650838253e760853eb5e2
Paulo Vidal <u1d4lp@gmail.com>
2021-10-14 22:47:52 -0300

    Update Campaign.sol

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,10 +1,27 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
     Request[] public requests;
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    // modifier contributor() {
+    //     require(msg.sender in approvers);
+    //     _;
+    // }
     
     function Campaign(uint minimum) public {
         manager = msg.sender;
@@ -16,15 +33,22 @@ contract Campaign {
         approvers.push(msg.sender);
     }
     
-    function createRequest() restricted {}
-    
-    function approveRequest() {}
-    
-    function finalizeRequest() restricted {}
-    
-    modifier restricted() {
-        require(msg.sender == manager);
-        _;
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false
+        });
+        
+        // a worse way of doing it: Request(description, value, recipient, false);
+        
+        requests.push(newRequest);
     }
-
+    
+    // function approveRequest() contributor {}
+    
+    function finalizeRequest() public restricted {
+        
+    }
 }

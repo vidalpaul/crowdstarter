591b363c1b8ed60f21a7bf7fad36a9ec98fdc8fb
Paulo Vidal <u1d4lp@gmail.com>
2021-10-20 21:12:20 -0300

    fix compiled contracts filenames

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -17,7 +17,7 @@ const output = solc.compile(source, 1).contracts;
 fs.ensureDirSync(buildPath);
 for (let contract in output) {
    fs.outputJSONSync(
-      path.resolve(buildPath, contract + '.json'),
+      path.resolve(buildPath, contract.replace(':', '') + '.json'),
       output[contract]
    );
 }

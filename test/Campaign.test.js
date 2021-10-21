1cb738a4f9b5bd645e22fa21fd08ac7026df5be9
Paulo Vidal <u1d4lp@gmail.com>
2021-10-21 12:18:51 -0300

    implement test to check if it allows people to contribute money and mark them as approvers

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -39,4 +39,14 @@ describe('Campaigns', () => {
       const manager = await campaign.methods.manager().call();
       assert.equal(accounts[0], manager);
    });
+
+   it('allows people to contribute money and mark them as approvers', async () => {
+      await campaign.methods
+         .contribute()
+         .send({ from: accounts[1], value: '200' });
+      const isContributor = await campaign.methods
+         .approvers(accounts[1])
+         .call();
+      assert(isContributor);
+   });
 });

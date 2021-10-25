5b94d067a0238750c888b47f2e62dfa5a5e942ea
Paulo Vidal <u1d4lp@gmail.com>
2021-10-25 15:20:47 -0300

    show campaigns list on index page

---
+++ components/Layout.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Layout = (props) => {};
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'next';
+import { Button, Card } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import 'semantic-ui-css/semantic.min.css';
 
 export default class CampaignIndex extends Component {
-   async componentDidMount() {
+   static async getInitialProps() {
       const campaigns = await factory.methods.getDeployedCampaigns().call();
-      console.log(campaigns);
+      return { campaigns };
+   }
+
+   renderCampaigns() {
+      const items = this.props.campaigns.map((address) => {
+         return {
+            header: address,
+            description: <a>View Campaign</a>,
+            fluid: true,
+         };
+      });
+      return <Card.Group items={items} />;
    }
 
    render() {
-      return <>Campaigns</>;
+      return (
+         <>
+            <div>
+               <h3>Open Campaigns</h3>
+               {this.renderCampaigns()}
+               <Button content='Create campaign' icon='add circle' primary />
+            </div>
+         </>
+      );
    }
 }

23a7120bc4e3ac7ae8781c16cf1444a3c6e8e7f6
Paulo Vidal <u1d4lp@gmail.com>
2021-10-26 12:35:12 -0300

    implement header navigation

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 const Header = (props) => {
    return (
       <Menu style={{ marginTop: '10px' }}>
-         <Menu.Item>CrowdStarter</Menu.Item>
+         <Link route='/'>
+            <a className='item'>CrowdStarter</a>
+         </Link>
          <Menu.Menu position='right'>
-            <Menu.Item>Campaigns</Menu.Item>
-            <Menu.Item>+</Menu.Item>
+            <Link route='/campaigns'>
+               <a className='item'>Campaigns</a>
+            </Link>
+            <Link route='/campaigns/new'>
+               <a className='item'>+</a>
+            </Link>
          </Menu.Menu>
       </Menu>
    );
--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,7 @@ import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import web3 from '../../ethereum/web3';
 import factory from '../../ethereum/factory';
+import { Router } from '../../routes';
 
 export class CampaignNew extends Component {
    state = { minimumContribution: '0', errorMessage: '', loading: false };
@@ -15,6 +16,7 @@ export class CampaignNew extends Component {
          await factory.methods
             .createCampaign(this.state.minimumContribution)
             .send({ from: accounts[0] });
+         Router.pushRoute('/');
       } catch (err) {
          this.setState({ errorMessage: err.message });
       }

3a52b32a53ea3631abb2bdd8d1e8feb4d2d87cb8
Paulo Vidal <u1d4lp@gmail.com>
2021-10-27 18:16:43 -0300

    finish contribute flow

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
-   state = { value: '', loading: false };
+   state = { value: '', loading: false, errorMessage: '' };
 
-   onSubmit = (event) => {
+   onSubmit = async (event) => {
       event.preventDefault();
-      loading: true;
+      this.setState({ loading: true, errorMessage: '' });
       const campaign = Campaign(this.props.address);
       try {
-      } catch (err) {}
-      loading: false;
+         const accounts = await web3.eth.getAccounts();
+         await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei(this.state.value, 'ether'),
+         });
+         Router.replaceRoute(`/campaigns/${this.props.address}`);
+      } catch (err) {
+         this.setState({ errorMessage: err.message });
+      }
+      this.setState({ loading: false, value: '' });
    };
 
    render() {
       return (
-         <Form onSubmit={this.onSubmit}>
+         <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
             <Form.Field>
                <label>Amount to contribute</label>
                <Input
@@ -27,7 +37,14 @@ class ContributeForm extends Component {
                   label='ether'
                   labelPosition='right'
                />
-               <Button primary>Contribute</Button>
+               <Message
+                  error
+                  header='Oops!'
+                  content={this.state.errorMessage}
+               />
+               <Button primary loading={this.state.loading}>
+                  Contribute
+               </Button>
             </Form.Field>
          </Form>
       );
